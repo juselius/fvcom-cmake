392916a546a0c1da56192727f4776eebd1198998
Jonas Juselius <jonas.juselius@uit.no>
2013-12-11 10:59:32 +0100

    Added attribution and example usage.

--- cmake/AddPrefix.cmake
+++ cmake/AddPrefix.cmake
@@ -1,8 +1,15 @@
-function (add_prefix var pfix filelist)
-    foreach(i ${filelist})
+# Prefix a variable or a list with a string.
+#
+# set (foo a b c)
+# add_prefix(bar ../ "${foo}")
+#
+# jonas.juselius@uit.no 2013
+#
+
+function (add_prefix var pfix lst)
+    foreach(i ${lst})
         set (f ${f} ${pfix}/${i})
     endforeach()
     set (${var} ${f} PARENT_SCOPE)
 endfunction()
-
 
